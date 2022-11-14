85b51badfdad373316ad366e16544d904f8b3842
LazyDennis <xingfang.xu@foxmail.com>
2022-11-14 22:15:52 +0800

    Change setting file to json; Add logging.

--- DnspodApi.py
+++ DnspodApi.py
@@ -1,4 +1,5 @@
 import requests
+from logging import Logger, getLogger
 
 
 class DnspodApi:
@@ -6,7 +7,11 @@ class DnspodApi:
     URL = r'https://dnsapi.cn/'
     API = ('Domain.Info', 'Record.Line.Category', 'Record.List', 'Record.Ddns')
 
-    def __init__(self, _domain: str = '', _token: str = ''):
+    def __init__(self, _domain: str = '', _token: str = '', __logger : Logger = None):
+        if __logger:
+            self.__logger: Logger = __logger
+        else:
+            self.__logger: Logger = getLogger()
         self.__domain: str = _domain
         self.__token: str = _token
         self.__result: dict = {}
@@ -47,15 +52,21 @@ class DnspodApi:
             self.__data.update({'format': 'json'})
 
         if len(_api):
-            res = requests.post(self.URL + _api, self.__data)
+            try:
+                self.__logger.debug(
+                    'Posting url: %s, data: %s', self.URL + _api, self.__data)
+                res = requests.post(self.URL + _api, self.__data)
+            except  requests.exceptions.RequestException as e:
+                self.__logger.error(e)
+                self.__logger.info('Program terminated.')
+                exit(0)
 
         import json
         self.__result = json.loads(res.text)
 
         return self.__result['status'].copy()
 
-    def __Post(self, _mode: int, _target_key_name: str, _target_list: list[dict], 
-                _preset_keys: tuple, **kwargs):
+    def __Post(self, _mode: int, _target_key_name: str, _preset_keys: tuple, **kwargs):
 
         temp_data = self.__data.copy()
 
@@ -66,19 +77,25 @@ class DnspodApi:
         res = self.Post(self.API[_mode])
 
         if res['code'] == '1' and _target_key_name in self.__result:
-            _target_list = self.__result[_target_key_name].copy()
+            target_list = self.__result[_target_key_name].copy()
 
-        self.__data = temp_data.copy()
+        self.__data = temp_data
 
-        return res, _target_list
+        return res, target_list
 
     def UpdateLineList(self, category_names: list[str] = [u'默认']):
         '''
         category_names:     list[str]   线路大类
         '''
         PRESET_KEYS = ['category_names']
-        ret, self.__line_list = self.__Post(1, 'data', self.__line_list, PRESET_KEYS, 
+        self.__logger.info('Updating line list by %s', category_names)
+        ret, self.__line_list = self.__Post(1, 'data', PRESET_KEYS, 
                 **{PRESET_KEYS[0]: category_names})
+        if ret['code'] == '1':
+            self.__logger.info('Update line list successfully.')
+        else:
+            self.__logger.info('Failed update line list with return code %s', ret['code'])
+            self.__logger.debug('Error message: %s', ret['message'])
         return ret, self.__line_list
 
     def UpdateRecordList(self, **kwargs):
@@ -94,9 +111,16 @@ class DnspodApi:
         '''
         PRESET_KEYS = ('offset', 'length', 'sub_domain',
                        'record_type', 'record_line', 'record_line_id', 'keyword')
+        
+        self.__logger.info('Updating record list...')
+        ret, self.__record_list = self.__Post(2, 'records', PRESET_KEYS, **kwargs)
+        
+        if ret['code'] == '1':
+            self.__logger.info('Update record list successfully.')
+        else:
+            self.__logger.info('Failed update record list with return code %s', ret['code'])
+            self.__logger.debug('Error message: %s', ret['message'])
 
-        ret, self.__record_list = self.__Post(2, 'records', self.__record_list, 
-                                                PRESET_KEYS, **kwargs)
         return ret, self.__record_list
 
     def UpdateDns(self, **kwargs):
@@ -110,7 +134,15 @@ class DnspodApi:
         '''
         PRESET_KEYS = ('record_id', 'sub_domain', 'record_line',
                        'record_line_id', 'value', 'ttl')
+        
+        self.__logger.info('Updating record...')
 
-        record_result : dict = {}
+        ret, record_result = self.__Post(3, 'record', PRESET_KEYS, **kwargs)
 
-        return self.__Post(3, 'record', record_result, PRESET_KEYS, **kwargs)
+        if ret['code'] == '1':
+            self.__logger.info('Update record successfully.')
+        else:
+            self.__logger.info('Failed update line list with return code %s', ret['code'])
+            self.__logger.debug('Error message: %s', ret['message'])
+
+        return ret, record_result
--- __main__.py
+++ __main__.py
@@ -1,68 +1,124 @@
 import subprocess
 import re
-import config
-from config import PATTERN, COMMAND, TOKEN, DOMAIN, SUB_DOMAIN, PING_COMMAND
 from DnspodApi import DnspodApi
 
-def GetRemoteIpAddrs(_dnspod : DnspodApi,sub_domain: str, 
-                    record_type: str = config.IPV6, **kwargs):
+
+def main():
+    global GlobalLogger
+    ddns_conf, log_conf = GetConfig()
+    GlobalLogger = GetLogger(log_conf)
+    platform_setting = GetPlatformSetting(ddns_conf)
+    dnspod = DnspodApi(ddns_conf['domain'], ddns_conf['token'], GlobalLogger)
+    local_ipaddrs = GetLocalIpAddrs(
+        platform_setting['local_ip_getcommand'] + ' ' +
+        platform_setting['local_ethernet_device_name'], 
+        platform_setting['local_ip_match_pattern'])
+    remote_ipaddrs = []
+    local_ip : str = ''
+    remote_ipaddrs = GetRemoteIpAddrs(dnspod, ddns_conf['sub_domain'])
+
+    if remote_ipaddrs:
+        for local_ip in local_ipaddrs:
+            if local_ip in remote_ipaddrs:
+                GlobalLogger.info("Remote ip address is the same as the local's.")
+                GlobalLogger.info('Program terminated.')
+                exit(0)
+            elif TestIp(platform_setting['local_ip_testcommand'], local_ip):
+                break
+            else:
+                local_ip = None
+                
+
+    # ret, record_list = dnspod.UpdateRecordList(sub_domain=SUB_DOMAIN)
+    if local_ip:
+        GlobalLogger.info('Local ip address is: %s', local_ip)
+        record_list = dnspod.GetRecordList()
+
+        for record in record_list:
+            ret, res = dnspod.UpdateDns(record_id=record['id'], 
+                                            record_line_id=record['line_id'],
+                                            sub_domain=ddns_conf['sub_domain'],
+                                            value=local_ip
+                                            )  
+    else:
+        GlobalLogger.info('No available local ip.')
+
+
+def GetConfig(config_file = r'.\config.json'):
+    import json, os, time
+    from datetime import datetime
+    ddns_config = None
+    log_config = None
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as fp:
+            json_file = fp.read()
+            config = json.loads(json_file)
+        if 'ddns_conf' in config:
+            ddns_config = config['ddns_conf']
+        if 'log_conf' in config:
+            log_config = config['log_conf']
+        return ddns_config, log_config
+    else:
+        print(datetime.now().strftime("%y/%m/%d %H:%M:%S.%f"), ": Config file ", config_file, " does not exists.\nProgram terminated.")
+        exit(0)
+
+def GetLogger(_log_config):
+    import logging
+    import logging.config
+
+    logging.config.dictConfig(_log_config)
+    return logging.getLogger("ddns_info")
+
+def GetPlatformSetting(_ddns_config):
+    import sys
+    curr_platform = sys.platform
+    GlobalLogger.info('Current platform is %s', curr_platform)
+    return _ddns_config['platform_setting'][curr_platform]
+
+def GetRemoteIpAddrs(_dnspod : DnspodApi, sub_domain: str, 
+                    record_type: str = 'AAAA', **kwargs):
+    GlobalLogger.info('Getting remote ip adress for %s', 
+                      sub_domain + '.' + _dnspod.GetDomain())
     ret, record_list = _dnspod.UpdateRecordList(sub_domain=sub_domain, 
                                                 record_type=record_type,
                                                 **kwargs)
     ipaddrs = []                            
     if ret['code'] == '1'and record_list:
         for record in record_list:
-            ipaddrs.append(record['value'])
+            ipaddrs.append(record['value'])    
+        GlobalLogger.info('Successfully got remote ip addresses: %s', ipaddrs)
+    else:
+        GlobalLogger.info('Failed to get remote ip addresses with return code(%s)', ret['code'])
+        GlobalLogger.debug('Error message: %s', ret['message'])
+    
     return ipaddrs
 
 
 def GetLocalIpAddrs(_command, _pattern):
+    GlobalLogger.info('Getting local ip.')
+    GlobalLogger.debug('Using comand (%s) to get local ip.', _command[:-1:])
     ret, val = subprocess.getstatusoutput(_command)
-    ipaddrs = re.findall(_pattern, val)
+    ipaddrs = None
+    if ret == 0:
+        ipaddrs = re.findall(_pattern, val)
+        GlobalLogger.info('Successfully got local addresses: %s', ipaddrs)
+    else:
+        GlobalLogger.info('Faield to get local addresses.')
+        GlobalLogger.debug('Error message: %s', val)
     return ipaddrs
 
-def TestIp(_target_ip : str):
-    test_command = PING_COMMAND + _target_ip[:_target_ip.rfind(':') + 1:] + '1'
+def TestIp(_command, _target_ip : str):
+    GlobalLogger.info('Testing ip: %s', _target_ip)
+    test_command = _command + _target_ip[:_target_ip.rfind(':') + 1:] + '1'
     ret, val = subprocess.getstatusoutput(test_command)
     res = re.findall(r'Lost = (\d)',val)
-    return res != '4'
-    
-
-def main():
-    dnspod = DnspodApi(DOMAIN, TOKEN)
-    local_ipaddrs = GetLocalIpAddrs(COMMAND, PATTERN)
-    remote_ipaddrs = []
-    try:
-        remote_ipaddrs = GetRemoteIpAddrs(dnspod, SUB_DOMAIN)
-    except Exception as e:
-        print(e)
-
-    for local_ip in local_ipaddrs:
-        if local_ip in remote_ipaddrs:
-            print("Remote IP Address is the same the local's.")
-            exit(0)
-        elif TestIp(local_ip):
-            break
-        else:
-            local_ip = None
-                
-
-    # ret, record_list = dnspod.UpdateRecordList(sub_domain=SUB_DOMAIN)
-    if local_ip:
-        print('Local ip is: ', local_ip)
-        record_list = dnspod.GetRecordList()
-        print(record_list)
-
-        for record in record_list:
-            if record['type'] == config.IPV6:
-                ret, res = dnspod.UpdateDns(record_id=record['id'], 
-                                            record_line_id=record['line_id'],
-                                            sub_domain=SUB_DOMAIN,
-                                            value=local_ip
-                                            )  
-                print(ret, res)
+    if res != '4':
+        GlobalLogger.info('Ip is valid.')
+        return True
     else:
-        print('No available local ip.')
+        GlobalLogger.info('Ip is not valid.')
+        return False
+    
 
 if __name__ == '__main__':
     main()
